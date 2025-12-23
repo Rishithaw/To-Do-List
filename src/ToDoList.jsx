b61283573fe8d6bda8772feca3ae2d1a259c8e95
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-12-23 10:58:18 -0600

    Added Required Fuctions

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,6 +3,26 @@ import React, { useState } from 'react';
 function ToDoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+
+  }
+
+  function addTask() {
+
+  }
+
+  function deleteTask() {
+
+  }
+
+  function moveTaskUp(index) {
+
+  }
+
+  function moveTaskDown(index) {
+
+  }
 }
 
 export default ToDoList;
