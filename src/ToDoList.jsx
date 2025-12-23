314572b33370fe7865c906d000b3f59667155c7b
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-12-23 11:01:21 -0600

    Added and Handled Input

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -5,7 +5,7 @@ function ToDoList() {
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event) {
-
+    setNewTask(event.target.value);
   }
 
   function addTask() {
@@ -23,6 +23,22 @@ function ToDoList() {
   function moveTaskDown(index) {
 
   }
+
+  return (
+    <div className="to-do-list">
+      <h1>To-Do List</h1>
+
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleInputChange}
+          placeholder="Enter a new task"
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+    </div>
+  );
 }
 
 export default ToDoList;
