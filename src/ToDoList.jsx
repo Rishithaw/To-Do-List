41a4460a1534f905f2d9fc1ca9d13b31cc653da3
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-12-23 11:16:35 -0600

    Added HTML

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function ToDoList() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(["Breakfast", "Lunch", "Dinner"]);
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event) {
@@ -37,6 +37,17 @@ function ToDoList() {
         />
         <button onClick={addTask}>Add Task</button>
       </div>
+
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+            <button className="move-up-button" onClick={() => moveTaskUp(index)}>Move Up</button>
+            <button className="move-down-button" onClick={() => moveTaskDown(index)}>Move Down</button>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
